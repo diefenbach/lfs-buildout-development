73657218df488e74eb0fe53ba833c11fd2c7aee4
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-01-04 14:47:01 +0100

    Added django.contrib.redirects to MIDDLEWARE_CLASSES

--- lfs_project/settings.py
+++ lfs_project/settings.py
@@ -65,8 +65,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "django.contrib.redirects.middleware.RedirectFallbackMiddleware",    
     "lfs.utils.middleware.AJAXSimpleExceptionResponse",
     "pagination.middleware.PaginationMiddleware",
+
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     # "django.middleware.cache.FetchFromCacheMiddleware",
     "lfs.utils.middleware.ProfileMiddleware",
