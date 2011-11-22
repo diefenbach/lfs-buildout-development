22bb963c993ba6d183c8046711b5a7d44e78fac1
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-11-22 14:48:27 +0100

    Set COMPRESS_ENABLED to False

--- lfs_project/settings.py
+++ lfs_project/settings.py
@@ -5,7 +5,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 TESTING = False
-COMPRESS_ENABLED = True
+COMPRESS_ENABLED = False
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
--- lfs_project/urls.py
+++ lfs_project/urls.py
@@ -14,13 +14,14 @@ urlpatterns = patterns("",
     (r'^manage/', include('lfs.manage.urls')),
 )
 
-urlpatterns += patterns("",    
+urlpatterns += patterns("",
+    (r'', include('lfs_rest.urls')),
     (r'^reviews/', include('reviews.urls')),
     (r'^paypal/ipn/', include('paypal.standard.ipn.urls')),
     (r'^paypal/pdt/', include('paypal.standard.pdt.urls')),
 )
 
-urlpatterns += patterns("",    
+urlpatterns += patterns("",
     (r'^admin/', include(admin.site.urls)),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
 )
