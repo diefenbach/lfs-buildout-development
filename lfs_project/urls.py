d0b896c1eaddd10de36a008f313777ccaaa4845b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-01-20 07:24:13 +0100

    Added import local_urls.py.

--- lfs_project/urls.py
+++ lfs_project/urls.py
@@ -24,3 +24,8 @@ urlpatterns += patterns("",
     (r'^admin/', include(admin.site.urls)),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
 )
+
+try:
+    from local_urls import *
+except ImportError:
+    pass
